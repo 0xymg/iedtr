f7cc994a616fba45379ff9200f6378bfdbc7b209
Yunus Melih Gozutok <ymgozutok@gmail.com>
2024-11-18 03:50:04 +0100

    initial commit with sketch

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,69 @@
-import Image from "next/image";
+import { AppSidebar } from "@/components/app-sidebar"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { ChevronRight, Download, Fullscreen, Save, Eraser, Square, Type, Brush, Settings, Moon, Minimize2, Crop, Trash, Sparkles, Scaling, Upload, Undo, Redo, ZoomIn, ZoomOut } from "lucide-react"
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="sticky top-0 flex shrink-0 items-center justify-between gap-2 border-b bg-background p-4">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="flex gap-2">
+            <Button variant="outline" size="icon">
+              <Undo />
+            </Button>
+            <Button variant="outline" size="icon">
+              <Redo />
+            </Button>
+            <Separator orientation="vertical" className="mx-2 h-8" />
+            <Button variant="outline" size="icon">
+              <ZoomIn />
+            </Button>
+            <Button variant="outline" size="icon">
+              <ZoomOut />
+            </Button>
+            <Separator orientation="vertical" className="mx-2 h-8" />
+            <Button variant="outline" size="icon">
+              <Fullscreen />
+            </Button>
+            <Button variant="outline" size="icon">
+              <Trash />
+            </Button>
+
+          </div>
+
+          <div className="flex gap-2">
+
+            <Button variant="outline" size="icon">
+              <Download />
+            </Button>
+            <Button variant="outline" size="icon">
+              <Settings />
+            </Button>
+
+
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 items-center justify-center">
+          <div className="flex w-full border rounded-lg p-4 h-full">CANVAS</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <footer className="sticky bottom-0 flex shrink-1 items-center justify-center gap-2 border-t h-48 bg-gray-50 p-4">
+
+          <div></div>
+
+        </footer>
+
+        <footer className="sticky bottom-0 flex shrink-0 items-center  border-t bg-black-100 text-gray-400 text-xs px-1 py-0.5  ">
+          <div>All rights reserved...</div>
+        </footer>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
